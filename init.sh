927d2e8fa40c3d550b61c1c66f1c17dd2be4b610
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-05 02:16:53 -0500

    Initialize reflector when localizing

--- init.sh
+++ init.sh
@@ -6,8 +6,6 @@ chmod 700 ~/.ssh
 chmod 600 ~/.ssh/authorized_keys
 curl https://gitlab.com/jmcantrell.keys >~/.ssh/authorized_keys
 
-reflector --save /etc/pacman.d/mirrorlist --country US --protocol https --latest 5
-
 pacman -Sy --noconfirm git
 
 git clone https://gitlab.com/jmcantrell/bootstrap-arch.git ~/bootstrap

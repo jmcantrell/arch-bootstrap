e98d1ac78e8d5b0c0c0c1ff67eb5bdfa869c9ee9
Jeremy Cantrell <jmcantrell@gmail.com>
2021-08-07 21:55:47 -0500

    Silence curl downloads

--- init.sh
+++ init.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
+curl -sL https://gitlab.com/jmcantrell.keys >~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
-curl https://gitlab.com/jmcantrell.keys >~/.ssh/authorized_keys
 
 cd ~
 
 archive_name=bootstrap-arch-master
-curl -O https://gitlab.com/jmcantrell/bootstrap-arch/-/archive/master/"$archive_name".tar.gz
+curl -sLO https://gitlab.com/jmcantrell/bootstrap-arch/-/archive/master/"$archive_name".tar.gz
 tar -x -f "$archive_name".tar.gz
 
 cd ~/"$archive_name"
